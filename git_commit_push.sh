c7c5f7985c045059e910d8a61b7b11f16c0085e3
votientu <votientu@gmail.com>
2020-07-12 20:55:58 +0200

    add function

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -1,3 +1,9 @@
+# Save change in local repository
 git add .
 git commit -m "add function"
+
+# Pull it first to merge possible changes of others
+git pull 
+
+# Synchronise with remote repository
 git push
